e78107527cc1ca8407330a9a425fbc1aa63a4cef
Dayne Broderson <broderson@gmail.com>
2024-02-25 17:00:51 -0900

    initial version

---
+++ lib/env.sh
@@ -0,0 +1,5 @@
+SCRIPT_CACHE_DIR=$HOME/.local/share/cl-picoruby
+UF2_PICORUBY_FILE="UF2_PICORUBY_FILE unknown for d.pico"
+PICORUBY_YAML=.uf2_picoruby.yaml
+PICORUBY_GITHUB_USER=picoruby
+PICORUBY_GITHUB_REPO=R2P2
