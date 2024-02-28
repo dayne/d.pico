b89e1377aef6c747b24ca6691d585c8ced1f5e80
Dayne Broderson <broderson@gmail.com>
2024-02-27 21:25:31 -0900

    switch share to d.pico folder

--- lib/env.sh
+++ lib/env.sh
@@ -1,4 +1,4 @@
-SCRIPT_CACHE_DIR=$HOME/.local/share/cl-picoruby
+SCRIPT_CACHE_DIR=$HOME/.local/share/d.pico
 UF2_PICORUBY_FILE="UF2_PICORUBY_FILE unknown for d.pico"
 PICORUBY_YAML=.uf2_picoruby.yaml
 PICORUBY_GITHUB_USER=picoruby
